286018dec0aba66326405b80dee9c9ae88193c5b
Benedek Varga <b.varga@koin.hu>
2019-04-04 17:41:54 +0200

    Inits project

--- app/src/main/java/com/benedekvarga/weather/view/MainActivity.kt
+++ app/src/main/java/com/benedekvarga/weather/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.benedekvarga.weather
+package com.benedekvarga.weather.view
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.benedekvarga.weather.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/benedekvarga/weather/view/WeatherApp.kt
@@ -0,0 +1,10 @@
+package com.benedekvarga.weather.view
+
+import androidx.multidex.MultiDexApplication
+
+class WeatherApp: MultiDexApplication() {
+    // First entry point
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
